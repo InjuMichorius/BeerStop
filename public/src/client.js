c1786585476c66c39030a4c83fb4bca928c732e5
partychickenking <partychickenking@gmail.com>
2021-04-12 15:58:02 +0200

    Aded html and started on styling/scripting

--- public/src/client.js
+++ public/src/client.js
@@ -1,19 +1,18 @@
 var socket = io()
-var messages = document.querySelector('section ul')
-var input = document.querySelector('input')
+var form = document.getElementById('account')
+var username = document.getElementById('username')
+var roomId = document.getElementById('room-id')
 
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault()
-  if (input.value) {
-    socket.emit('message', input.value)
-    input.value = ''
-  }
+//Fire function when user sends message
+form.addEventListener('submit', (event) => {
+    localStorage.setItem("Username", username.value)
+    localStorage.setItem("roomId", roomId.value)
+    console.log(localStorage)
 })
 
-socket.on('message', function(message) {
-  var element = document.createElement('li')
-  element.textContent = message
-  messages.appendChild(element)
-  messages.scrollTop = messages.scrollHeight
+socket.on('message', function (message) {
+    var element = document.createElement('li')
+    element.textContent = message
+    messages.appendChild(element)
+    messages.scrollTop = messages.scrollHeight
 })
-
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
+//Something with bodyparser
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+//Set default path to public
 app.use(express.static(path.resolve('public')))
 
+//Fires a function on user connection with the socket
 io.on('connection', (socket) => {
+  
   console.log('New user connected')
 
   socket.on('message', (message) => {
-    if (message === 'snorkel') {
-        io.emit('message', "User guessed the word correctly!")
-    } else {
-        io.emit('message', message)
-    }
+    io.emit('message', message)
   })
-
-  socket.broadcast.emit('message', 'A user has joined the chat')
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
@@ -26,6 +29,7 @@ io.on('connection', (socket) => {
   })
 })
 
+//Our node app is live!
 http.listen(port, () => {
-  console.log('listening on port ', port)
+  console.log(`Listening on http://localhost:${port}`)
 })
