95a35336a20f65253fcec289250c7b1de16bf580
partychickenking <partychickenking@gmail.com>
2021-04-13 00:24:36 +0200

    Last push berfore sleep

--- public/src/client.js
+++ public/src/client.js
@@ -2,8 +2,8 @@ document.getElementById('start-game').addEventListener('click', startGame)
 
 function startGame() {
     const body = document.querySelector("body")
-    body.insertAdjacentHTML("afterend", "<h1>Red or black?",
-    "<button>Red</button>", "<button>Black</button>"
+    body.innerHTML = ""
+    body.insertAdjacentHTML("afterend", "<span>Username</span><h1>Red or black?<img src='./img/user.png' alt='User profile'><button>Red</button><button>Black</button>"
 )}
 
 console.log('Hello world!')
